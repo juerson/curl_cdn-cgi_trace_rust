40af46c052f5828d5f75ce13b5ee5d253dec629b
Juerson <zhangrson@163.com>
2024-11-23 08:02:11 +0800

    update

--- src/utils/logger.rs
+++ src/utils/logger.rs
@@ -1,18 +1,19 @@
-use std::io;
-
 // 初始化日志（设置日志格式）
 pub fn init_logger() -> Result<(), fern::InitError> {
-    fern::Dispatch::new()
+    fern::Dispatch
+        ::new()
         .format(|out, message, record| {
-            out.finish(format_args!(
-                "{} {:<5}{}",
-                chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
-                record.level(),
-                message
-            ))
+            out.finish(
+                format_args!(
+                    "{} {:<5}{}",
+                    chrono::Local::now().format("%H:%M:%S%.3f"),
+                    record.level(),
+                    message
+                )
+            )
         })
         .level(log::LevelFilter::Info)
-        .chain(io::stdout())
+        .chain(std::io::stdout())
         .apply()?;
     Ok(())
 }
